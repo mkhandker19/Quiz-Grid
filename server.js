14b737b9eb4cf6d0c993e0ced654498663b5c706
Mahin Khandker <56740284+mkhandker19@users.noreply.github.com>
2025-12-02 01:43:04 -0500

    Mongodb added

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-// Quiz App Server
+
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const connectDB = require('./config/database');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from public directory
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Basic route handlers (placeholder)
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// API routes (placeholder - will be implemented in later commits)
+// API routes
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Server is running!' });
 });

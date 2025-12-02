6c69ab480b4de1398449fedeaa046039b7a0be9d
Mahin Khandker <56740284+mkhandker19@users.noreply.github.com>
2025-12-02 01:34:49 -0500

    Express server running

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 // Quiz App Server
-// This file will be set up in the next commit
-
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Basic server setup - will be expanded in next commits
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic route handlers (placeholder)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// API routes (placeholder - will be implemented in later commits)
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Server is running!' });
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Visit http://localhost:${PORT} to see the app`);
 });
 
